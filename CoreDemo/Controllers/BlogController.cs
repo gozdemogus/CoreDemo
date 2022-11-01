0c3d6a6a589827edb8af4066594437ee4b055bbd
gozdemogus <gozdemog@gmail.com>
2022-11-01 20:44:44 +0000

    read more

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
         public IActionResult Index()
         {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+
+        public IActionResult BlogDetails(int id)
+        {
+
             return View();
         }
     }
