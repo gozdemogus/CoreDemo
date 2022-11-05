27a35ab6745b15e38a230d64ef422b6770b03f0a
gozdemogus <gozdemog@gmail.com>
2022-11-05 12:33:30 +0000

    login

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Writer name is required.");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az iki karakter girişi yapın");
+            RuleFor(x => x.WriterName).MinimumLength(50).WithMessage("Lütfen en az iki karakter girişi yapın");
+
+
+        }
+    }
+}
