4254d9a8e15dd032ecc814c9ad3e0cb516136401
gozdemogus <gozdemog@gmail.com>
2022-11-10 19:59:11 +0000

    dashboard

--- BusinessLayer/Concrete/WriterManager.cs
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -9,18 +9,44 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-	public class WriterManager : IWriterService
-	{
-		IWriterDal _writerdal;
+    public class WriterManager : IWriterService
+    {
+        IWriterDal _writerdal;
 
-		public WriterManager(IWriterDal writerDal)
-		{
-			_writerdal = writerDal;
-		}
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerdal = writerDal;
+        }
 
-		public void WriterAdd(Writer writer)
-		{
-			_writerdal.Insert(writer);
-		}
-	}
+        public Writer GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> GetWriterById(int id)
+        {
+            return _writerdal.GetList(x => x.WriterID == id);
+        }
+
+        public void TAdd(Writer t)
+        {
+            _writerdal.Insert(t);
+        }
+
+        public void TDelete(Writer t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Writer t)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
