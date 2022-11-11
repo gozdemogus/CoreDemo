ee0afa87e34b577db560fa145bd03732fe362fa0
gozdemogus <gozdemog@gmail.com>
2022-11-11 19:26:44 +0000

    notification

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public List<Message> GetList()
+        {
+           return _messageDal.GetList();
+        }
+
+        public List<Message> GetInboxListByWriter(string p)
+        {
+            return _messageDal.GetList(x=>x.Receiver.ToString() ==p );
+        }
+
+        public void TAdd(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Message t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
