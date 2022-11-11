c78661958cb64b02aea2f5bdb9c81c4a421cd1ff
gozdemogus <gozdemog@gmail.com>
2022-11-11 13:38:46 +0000

    update writer

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -16,7 +16,7 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez.");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez.");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az iki karakter girişi yapın");
-            RuleFor(x => x.WriterName).MinimumLength(50).WithMessage("Lütfen en az iki karakter girişi yapın");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter girişi yapın");
 
 
         }
--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,47 +1,90 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace CoreDemo.Controllers
 {
-	public class WriterController : Controller
-	{
-		WriterManager wm = new WriterManager(new EFWriterRepository());
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EFWriterRepository());
 
 
-		[Authorize]
+        [Authorize]
         public IActionResult Index()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
-		public IActionResult WriterProfile() { return View(); }
+        public IActionResult WriterProfile() { return View(); }
 
         public IActionResult WriterMail() { return View(); }
 
-		[AllowAnonymous]
+        [AllowAnonymous]
         public IActionResult Test() { return View(); }
 
         [AllowAnonymous]
         public PartialViewResult WriterNavbarPartial()
-		{
-			return PartialView();
-		}
+        {
+            return PartialView();
+        }
 
         [AllowAnonymous]
         public PartialViewResult WriterFooterPartial()
-		{
-			return PartialView();
-		}
+        {
+            return PartialView();
+        }
 
-		[AllowAnonymous]
-		public IActionResult WriterEditProfile()
-		{
-		 var writervalues = wm.TGetById(1);
-			return View(writervalues);
-		}
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var writervalues = wm.TGetById(1);
+            return View(writervalues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult results = wl.Validate(p);
+            if (results.IsValid)
+            {
+                wm.TUpdate(p);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+
+
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd(Writer p)
+        {
+            wm.TAdd(p);
+            return View();
+        }
+
+
+
     }
 }
