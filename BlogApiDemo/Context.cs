f81db450c1d766ed7a77d9f699dd433b416d70e2
gozdemogus <gozdemogus@hotmail.com>
2022-11-13 11:13:34 +0300

    api crud

---
+++ BlogApiDemo/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+	public class Context:DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:127.0.0.1,1433;Database=master2;MultipleActiveResultSets=true;User=SA;Password=MyPass@word;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
