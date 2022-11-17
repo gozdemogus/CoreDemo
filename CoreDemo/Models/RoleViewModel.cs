0ed9b1320fd2aeb061a920937f1f8eff925d8395
gozdemogus <gozdemogus@hotmail.com>
2022-11-17 13:25:04 +0300

    role list

---
+++ CoreDemo/Models/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage = "Enter role name.")]
+        public string Name { get; set; }
+
+    }
+}
+
